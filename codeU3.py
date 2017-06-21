ca1877d6d8a132f73d71bd42b0adeedd7b17579c
Luiza Sayfullina <sayfullina.luiza@gmail.com>
2017-06-21 12:23:22 +0300

    Refined by the first review round
    Comments are provided by @windo, @luxiya01  and @blmussati , thank you for your reviews!:)
    Could you please look again modified version?

--- codeU3.py
+++ codeU3.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 10 09:17:26 2017
-Exercise 3, version 1
+Exercise 3, version 2
 @author: Luiza
 """
 
@@ -9,30 +9,33 @@ from collections import deque
 import unittest
 import numpy as np
 
-def getPrefixes(word):
-
-    '''Returns the list of all prefixes of given word
-    including the word itself
-    '''
-
-    return set([word[0:n] for n in range(1, len(word)+1)])
-
 class Dictionary(object):
 
     '''Stores lower-case vocabulary and all its prefixes as a set
     Two functions isWord and isPrefix are defined to check if given word/prefix is found
-    Constructor needs only a non-empty word-list, prefixes found automatically
     '''
 
     def __init__(self, word_list):
-        if not word_list:
-            assert ValueError, 'Please provide non-empty list of words'
+
+        '''Initialization with a non-empty word-list
+        prefixes are found automatically
+        '''
+
+        assert word_list, 'Please provide a non-empty list of words'
         word_list = [w.lower() for w in word_list if w]
-        self.max_word_len = reduce(max, map(len, word_list))
+        self.max_word_len = max(map(len, word_list))
         self.words = set(word_list)
         self.word_prefixes = set()
         for word in self.words:
-            self.word_prefixes.update(getPrefixes(word))
+            self.word_prefixes.update(self.getPrefixes(word))
+
+    def getPrefixes(self, word):
+
+        '''Returns the list of all prefixes of given word
+        including the word itself
+        '''
+        
+        return set([word[0:n] for n in range(1, len(word)+1)])
 
     def isWord(self, word):
 
@@ -42,10 +45,7 @@ class Dictionary(object):
 
         if not isinstance(word, str):
             assert TypeError, 'Input should be a string'
-            return False
-        if word.lower() in self.words:
-            return True
-        return False
+        return word.lower() in self.words
 
     def isPrefix(self, prefix):
 
@@ -55,10 +55,7 @@ class Dictionary(object):
 
         if not isinstance(prefix, str):
             assert TypeError, 'Input should be a string'
-            return
-        if prefix.lower() in self.word_prefixes:
-            return True
-        return False
+        return prefix.lower() in self.word_prefixes
 
 class TestDictionary(unittest.TestCase):
 
@@ -76,16 +73,13 @@ class TestDictionary(unittest.TestCase):
         self.assertTrue(self.d.isWord('Cats'))
         self.assertTrue(self.d.isPrefix('DOGS'))
 
-    def test_normal_input(self):
+    def test_normal_input_isword(self):
         self.assertTrue(self.d.isWord('cats'))
-        self.assertTrue(self.d.isPrefix('car'))
-
         self.assertFalse(self.d.isWord('cat'))
-        self.assertFalse(self.d.isPrefix('cs'))
 
-    def test_nonstring_input(self):
-        self.assertRaises(self.d.isWord(['cat', 'cats']))
-        self.assertRaises(self.d.isPrefix(['cat', 'cats']))
+    def test_normal_input_isprefix(self):
+        self.assertTrue(self.d.isPrefix('car'))
+        self.assertFalse(self.d.isPrefix('cs'))
 
 class CharGrid(object):
 
@@ -95,12 +89,16 @@ class CharGrid(object):
     '''
 
     def __init__(self, char_matrix, word_list):
-        
-        '''Given char_matrix as 2D numpy array and a list of words
-        Checks whether we have a proper matrix and character values in it
-        and saves them along with matrix shape as attributes
+
+        '''Initialization of char matrix as 2D numpy array
+        checks whether we have a proper matrix and character values in it,
+        saves them along with matrix shape as attributes and dictionary
+
+        Args:
+            char_matrix: char_matrix as 2D numpy
+            word_list: a list of strings, dictionary
         '''
-        
+
         char_array = char_matrix.flatten() # flattening for checking matrix shape correctness
         for c in char_array:
             if isinstance(c, list):
@@ -113,44 +111,31 @@ class CharGrid(object):
         self.dict = Dictionary(word_list)
         (self.m, self.n) = np.shape(char_matrix)
 
-    def is_route_prefix(self, route):
+    def route_to_word(self, route):
 
-        '''Given a list of tuple coordinates (x,y) encoding route
+        '''Finds a word, corresponding to the given route
+
+        Args:
+            route: a list of (x,y) indices
+
         Returns:
-            True: if route gives string that is a prefix in our dictionary
-            False: otherwise
+            word, corresponding to this route
+            empty string if route is empty
         '''
 
-        if not route:
-            return False
         chars = [self.grid[x, y] for (x, y) in route]
-        prefix = ''.join(chars)
-        if self.dict.isPrefix(prefix):
-            return True
-        return False
-
-    def route_word(self, route):
-
-        '''Given a list of tuple coordinates (x,y) encoding route
-        Returns:
-            empty string, if corresponding string is not in the dictionary
-            word, otherwise
-        '''
-
-        if not route:
-            return ""
-        chars = [self.grid[x, y] for (x, y) in route]
-        word = ''.join(chars)
-        if self.dict.isWord(word):
-            return word
-        return ""
+        return ''.join(chars)
 
     def route_extend(self, route):
 
-        '''Given a route as list of (x,y) indices
+        '''Extends the route with 1 step into 8 possible directions
+
+        Args:
+            route: a list of (x,y) indices
+
         Returns:
             all possible routes where 1 step made in one of 8 directions
-            each route is a list of tuple coordinates 
+            each route is a list of tuple coordinates
         '''
 
         last_x = route[-1][0] #last route coordinate x
@@ -158,6 +143,15 @@ class CharGrid(object):
         cells_set = set(route)
         route_extended = []
 
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == j and i == 0:
+                    continue
+                if 0 <= last_x + i < self.m and 0 <= last_y + j < self.n\
+                and (last_x + i, last_y + j) not in cells_set:
+                    route_extended.append(route + [(last_x + i, last_y + j)])
+
+        ''' old long alternative
         if last_x + 1 < self.m and (last_x + 1, last_y) not in cells_set: #ensure we don't visit the same cell twice
             route_extended.append(route + [(last_x + 1, last_y)])
         if last_x + 1 < self.m and last_y + 1 < self.n and (last_x + 1, last_y + 1) not in cells_set:
@@ -174,12 +168,13 @@ class CharGrid(object):
             route_extended.append(route + [(last_x, last_y - 1)])
         if  last_x + 1 < self.m and last_y - 1 >= 0 and (last_x + 1, last_y - 1) not in cells_set:
             route_extended.append(route + [(last_x + 1, last_y - 1)])
+        '''
 
         return route_extended
 
     def search_words(self):
 
-        '''Return all words found in the grid from given dictionary
+        '''Return all words found in the grid from a given dictionary
         Optimized by stopping searching longer route than maximum length of the dictionary word
         '''
 
@@ -188,9 +183,9 @@ class CharGrid(object):
         words_found = set()
         while routes:
             route = routes.popleft() #queue functionality
-            if self.is_route_prefix(route):
-                word = self.route_word(route)
-                if word:
+            word = self.route_to_word(route)
+            if self.dict.isPrefix(word):
+                if self.dict.isWord(word):
                     words_found.add(str(word))
                 if len(route) < self.dict.max_word_len:
                     routes.extendleft(self.route_extend(route))
